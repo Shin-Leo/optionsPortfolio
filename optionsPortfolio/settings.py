aeeef8a6d9b3b53928fe1b57f2cf56517e6d7b31
Leo <leoshin@students.cs.ubc.ca>
2021-06-06 22:46:12 -0700

    initial set up for postgresql

--- optionsPortfolio/settings.py
+++ optionsPortfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'optionsPortfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
