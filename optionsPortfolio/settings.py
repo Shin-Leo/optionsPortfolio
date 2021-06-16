dda5a26b1dd6779c991e761fd542d01f1bc34881
Leo <54517654+Panporo@users.noreply.github.com>
2021-06-15 21:56:12 -0700

    added simple user registration page and connected to back end

--- optionsPortfolio/settings.py
+++ optionsPortfolio/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'register.apps.RegisterConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- optionsPortfolio/urls.py
+++ optionsPortfolio/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', v.register, name="portfolio-registration"),
     path('', include('optionsPF.urls')),
 ]
