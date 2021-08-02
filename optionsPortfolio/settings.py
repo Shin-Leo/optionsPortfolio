a78964d613e0b87c5d34e9ba4c23ec6515b4ab80
Leo Shin <54517654+Shin-Leo@users.noreply.github.com>
2021-08-01 22:20:52 -0700

    fixed static files link

--- optionsPortfolio/settings.py
+++ optionsPortfolio/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'checkstatic/static/')
+    os.path.join(BASE_DIR, 'optionsPF/static/')
 ]
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = "bootstrap4"
